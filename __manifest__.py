ea633653360c485703578a8489a4ff18db79690f
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-26 11:19:24 -0600

    INIT

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Inmeza Invoice',
+    'version': '12.0.0.1.0',
+    'author': 'HomebrewSoft',
+    'website': 'https://github.com/HomebrewSoft/inmeza_invoice',
+    'category': 'Operations',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'report/sale_report_templates.xml',
+    ],
+}
