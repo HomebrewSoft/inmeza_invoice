fcac092e098e0bb4f01b98f3b090faeb8581cfe2
Omar Saucedo <omaroso27@gmail.com>
2019-11-29 20:28:31 -0600

    [IMP] header, footer and content

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Inmeza Invoice',
-    'version': '12.0.0.1.0',
+    'version': '12.0.0.2.0',
     'author': 'HomebrewSoft',
     'website': 'https://github.com/HomebrewSoft/inmeza_invoice',
     'category': 'Operations',
